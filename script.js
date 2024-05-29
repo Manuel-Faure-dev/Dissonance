7495ed0fb62ad96416721de2656711f74c9d4133
Manuel26190 <manuelfaure26190@gmail.com>
2024-05-29 20:19:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Gestion de la NavBar disparaît / apparaît au mouvement / au click navbBar-link
+
+var prevScrollpos = scrollY
+window.onscroll = function () {
+    var currentScrollPos = scrollY
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('nav').style.top = '0'
+    } else {
+        document.getElementById('nav').style.top = '-82px'
+    }
+    prevScrollpos = currentScrollPos
+}
+
+// Gestion du scrollTop
+// function scrollToTop() {
+//     document.body.scrollTop = 0 // Pour les navigateurs Chrome, Firefox, IE et Opera
+//     document.documentElement.scrollTop = 0 // Pour Safari
+//     console.log('test click ')
+// }
